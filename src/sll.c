7f0fb65c144b83b8185b49f4a7bafc9e5cd6b7c7
netzego <github@netzego.de>
2023-05-24 00:53:21 +0200

    src: add add_list implementation

--- src/sll.c
+++ src/sll.c
@@ -1,3 +1,15 @@
 #include <stdlib.h>
 
 #include "sll.h"
+
+node *list_add(uint8_t value, node *next)
+{
+    node *nod = malloc(sizeof(node));
+
+    if (nod != NULL) { /* malloc succeeded  */
+        nod->value = value;
+        nod->next  = next;
+    }
+
+    return nod;
+}
