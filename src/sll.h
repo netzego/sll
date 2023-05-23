bcfbb7e2be1dd3f0df162bd18eafa664930ed880
netzego <github@netzego.de>
2023-05-24 00:51:33 +0200

    src: add list_add function declaration

--- src/sll.h
+++ src/sll.h
@@ -18,4 +18,25 @@ struct sllnode {
  */
 typedef struct sllnode node;
 
+/**
+ *  Function to create a list or prepend a new allocated node to an existing
+ *  list/node.  Create a new list by specify a NULL pointer as the next parameter.
+ *  Prepend to an existing list by passing the head (first node) of the list as
+ *  next parameter.  The function uses malloc to allocate memory for the new
+ *  node.  On failure a NULL pointer will be returned.
+ *
+ *  @param value    The value of the node
+ *  @param next     Pointer to the next node or NULL pointer if
+ *
+ *  @returns        On success returns a pointer to new the node structure.
+ *                  On failure a NULL pointer is returned.
+ *
+ *  @code
+ *      node *head;
+ *      head = list_add(13, NULL) // create a new list
+ *      head = list_add(12, head) // prepend a new node to the list
+ *
+ */
+node *list_add(uint8_t value, node *next);
+
 #endif /* end __SSL_H  */
